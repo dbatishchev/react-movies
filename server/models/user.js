95ef850c638ff866f2340432e03098aeb0bd9841
Dmitrii <dbatishchev@gmail.com>
2017-01-20 11:17:07 +0300

    refactor user

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,9 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
+import mongoose from "mongoose";
 
 const User = new mongoose.Schema({
     username: {
         type: String,
         unique: true,
-        required: true,
-    },
-    hashedPassword: {
-        type: String,
-        required: true,
-    },
-    salt: {
-        type: String,
         required: true,
     },
     created: {
@@ -21,27 +12,4 @@ const User = new mongoose.Schema({
     },
 });
 
-User.methods.encryptPassword = function (password) {
-    return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
-};
-
-User.virtual('userId')
-    .get(function () {
-        return this.id;
-    });
-
-User.virtual('password')
-    .set(function (password) {
-        this._plainPassword = password;
-        this.salt = crypto.randomBytes(32).toString('hex');
-        this.hashedPassword = this.encryptPassword(password);
-    })
-    .get(function () {
-        return this._plainPassword;
-    });
-
-User.methods.checkPassword = function (password) {
-    return this.encryptPassword(password) === this.hashedPassword;
-};
-
-module.exports = mongoose.model('User', User);
+module.exports = mongoose.model("User", User);
